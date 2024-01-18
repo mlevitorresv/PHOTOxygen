c3d54a867b2087d20f06f17091e27af1cd4a23ff
mlevitorresv <levitorres66@gmail.com>
2024-01-18 15:46:16 +0100

    FIX: header elements

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import './header.css'
-import { AccountBoxRounded } from "@mui/icons-material";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import HomeIcon from '@mui/icons-material/Home';
 import { Link } from "react-router-dom";
 
 export const Header = () => {
     return(
         <header className="header">
-            <Link to="/" className="link"><h1 className="header__title">PHOTOxygen</h1></Link>
-            <Link to="/my-photos" className="link"><AccountBoxRounded className="header__icon"/></Link>
+            <Link to="/" className="link"><HomeIcon className="header__icon"/></Link>
+            <h1 className="header__title">PHOTOxygen</h1>
+            <Link to="/my-photos" className="link"><FavoriteIcon className="header__icon"/></Link>
         </header>
     )
 }
